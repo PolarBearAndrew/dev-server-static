b3fb9d08025b7017fbabc4cebe1c261a5085c5db
Andrew <chenpoanandrew@gmail.com>
2015-07-24 15:51:32 +0800

    use config now

--- devServer/routes/index.js
+++ devServer/routes/index.js
@@ -13,7 +13,7 @@ router.post('/', function(req, res, next) {
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
