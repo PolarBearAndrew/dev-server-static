b2559f7bf1da34115ebe90e7a6c659540277140c
Andrew <chenpoanandrew@gmail.com>
2015-09-26 01:19:26 +0800

    update _admin page

--- devServer/public/asset/javascripts/_admin.js
+++ devServer/public/asset/javascripts/_admin.js
@@ -7,8 +7,26 @@ $(document).ready( function(){
       type: 'POST',
 
       success: function( result ){
+
         console.log('result', result)
-        $('#result').html( '建立頁面：' + JSON.stringify(result) );
+        // $('#result').html( '建立頁面：' + JSON.stringify(result) );
+
+        var list = result.list;
+        var output = '<br/><div class="ui inverted segment list-wrapper"> <div class="ui list inverted divided">  ';
+        var item = '<div class="item"><i class="html5 icon ui orange"></i> <div class="content"><a href="@url" target="_blank" class="header">@title</a> <div class="description">Build Success</div> </div> </div>';
+
+        for( var i = 0; i < list.length; i++ ){
+
+          if( i % 8 === 0 && i !== 0 ){ output += '</div></div> <div class="ui inverted segment list-wrapper"> <div class="ui list inverted divided">'; }
+
+          output += item.replace(/@url/, list[i].page + '.html' )
+                        .replace(/@title/, list[i].page);
+
+        }
+
+        $('#result').html( output + '</div></div>' );
+
+        console.log('output', output);
       },
 
       fail: function( err ){
